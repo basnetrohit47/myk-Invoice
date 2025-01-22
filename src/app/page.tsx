8c965e7dcd397f2193f6c17b233d9c8939c3c980
Roh <basnetrohit47@gmail.com>
2025-01-22 14:37:10 +0100

    debounce test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,41 @@
+"use client";
 import { InvoiceForm } from "@/components/invoice/form/InvoiceForm";
+import { useDebounce } from "@/hooks/useDebounce";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [text, setText] = useState("");
+  const debouncedValue = useDebounce(text);
+  useEffect(() => {
+    if (debouncedValue) {
+      console.log(debouncedValue);
+    }
+  }, [debouncedValue]);
+  const [debouncedText, setDebouncedText] = useState("");
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [text]);
+
+  useEffect(() => {
+    if (debouncedText) {
+      console.log("debounce text", debouncedText);
+    }
+  }, [debouncedText]);
+
   return (
     <div className="m-8">
+      <div>
+        <h2>Debounce</h2>
+        <div>
+          <input placeholder="text" onChange={(e) => setText(e.target.value)} />
+        </div>
+      </div>
       <InvoiceForm />
     </div>
   );
